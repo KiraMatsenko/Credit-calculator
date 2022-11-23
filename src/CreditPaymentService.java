8cf3a03a1f3b88d99959ded4082f6f7aa5ea0479
Kira <kira_pavlenko_13@mail.ru>
2022-11-24 01:19:05 +0300

    First commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
 
-    private double months;
-
+    private int months;
+    private double interestRate;
     public double calculate(int cost) {
-        return cost * (9.99 / (1 + 9.99) - months - 1);
+        return cost * (interestRate / (1 + interestRate) - months - 1);
     }
 }
